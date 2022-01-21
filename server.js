6fbaeccf1efabc25934d5bf044a7f81f7fd07d6d
Max Wang <maxwang051@gmail.com>
2022-01-21 15:42:07 -0800

    Add route for client

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 app.use(express.static('./client/build'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.get('/', (req, res) => {
   res.send('API Running');
 });
